ad99d708fe4dfdebb7efc71bc3db56ee3451a290
LOGESH.B <110714456+logesh200281@users.noreply.github.com>
2025-02-20 09:21:26 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        GoogleSans: "'Google Sans', sans-serif",
+        Poppins: "'Poppins', sans-serif"
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar-hide')
+  ],
+}
